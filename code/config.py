743ffc929d8cd08fe80853ee8998345ffe3ff423
Javi Naranjo-Alcazar <javi3naranjo@gmail.com>
2020-12-01 13:14:55 +0100

    adding config file in python mode

---
+++ code/config.py
@@ -0,0 +1,14 @@
+# data paths
+
+# audio representation hyperparameters
+freq_bands = 128
+
+# callbacks
+
+
+# training hyperparamteres
+loss_type = 'focal_loss'
+data_augmentation = 'mixup'
+epochs = 500
+mixup_alpha = 0.4
+batch_size = 32
