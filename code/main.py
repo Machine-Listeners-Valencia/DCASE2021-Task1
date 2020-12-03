8dc4fa0b3162d88e073bb9f8c6fb2bd3294ff3ab
Javi Naranjo-Alcazar <javi3naranjo@gmail.com>
2020-12-03 10:57:04 +0100

    adding compile model from config file

--- code/main.py
+++ code/main.py
@@ -2,28 +2,38 @@ import config
 from load_data import load_h5s
 from models import res_conv_standard_post_csse
 from focal_loss import categorical_focal_loss
-from utils import check_reshape_variable, check_model_depth
+from utils import check_reshape_variable, check_model_depth, check_alpha_list, check_loss_type
 
 check_reshape_variable(config.reshape_method)
 check_model_depth(config.n_filters, config.pools_size, config.dropouts_rate)
+check_loss_type(config.loss_type)
 
 X, Y, val_x, val_y = load_h5s(config.path)
 
 print('Training shape: {}'.format(X.shape))
 print('Validation shape: {}'.format(val_x.shape))
-#
+
 model = res_conv_standard_post_csse(X.shape[1], X.shape[2], X.shape[3], Y.shape[1],
                                     config.n_filters, config.pools_size, config.dropouts_rate, config.ratio,
                                     config.reshape_method, config.dense_layer,
                                     verbose=True)
-#
-# model.compile(loss=[categorical_focal_loss(alpha=[[.25, .25, .25, .25, .25, .25, .25, .25, .25, .25]],
-#                                            gamma=config.fl_gamma)],
-#               metrics=['categorical_accuracy'], optimizer='adam')
-#
-# if config.quick_test:
-#     epochs = 2
-# else:
-#     epochs = config.epochs
-#
-# model.fit(X, Y, validation_data=(val_x, val_y), batch_size=32, epochs=epochs)
+
+if type(config.fl_alpha) is not list:
+    alpha_list = [[config.fl_alpha] * Y.shape[1]]
+else:
+    alpha_list = config.fl_alpha
+    check_alpha_list(alpha_list, Y.shape[1])
+
+if config.loss_type == 'focal_loss':
+    model.compile(loss=[categorical_focal_loss(alpha=alpha_list, gamma=config.fl_gamma)],
+                  metrics=['categorical_accuracy'], optimizer='adam')
+
+elif config.loss_type == 'categorical_loss':
+    model.compile(loss='categorical_crossentropy', metrics=['categorical_accuracy'], optimizer='adam')
+
+if config.quick_test:
+    epochs = 2
+else:
+    epochs = config.epochs
+
+model.fit(X, Y, validation_data=(val_x, val_y), batch_size=config.batch_size, epochs=epochs)
--- code/utils.py
+++ code/utils.py
@@ -1,5 +1,4 @@
 def check_reshape_variable(reshape_method):
-
     possible_options = ['global_avg', 'global_max', 'flatten']
 
     if reshape_method not in possible_options:
@@ -8,7 +7,22 @@ def check_reshape_variable(reshape_method):
 
 
 def check_model_depth(n_filters, pools_size, dropouts_rate):
-
-    if len(n_filters) != len(pools_size) or len(n_filters) != len(dropouts_rate) or len(pools_size) != len(dropouts_rate):
+    if len(n_filters) != len(pools_size) or len(n_filters) != len(dropouts_rate) or len(pools_size) != len(
+            dropouts_rate):
         raise Exception('Lengths of list denoting the number of filters, dropouts rate and pool sizes do not match, '
                         'please check config file')
+
+
+def check_alpha_list(alpha_list, n_classes):
+    if len(alpha_list) != 1 or len(alpha_list[0]) != n_classes:
+        raise Exception('Alpha dimensions do not match. Please check that it has been defined with double list notation'
+                        ' [[]]. If that is the case, please check that is has the same positions '
+                        'as the number of classes')
+
+
+def check_loss_type(loss_type):
+    possible_options = ['focal_loss', 'categorical_loss']
+
+    if loss_type not in possible_options:
+        raise Exception('Defined loss type not available. Please check config file. '
+                        'Possible options are: {}'.format(possible_options))
